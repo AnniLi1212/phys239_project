220593a7ecddd26047dfacac9af57bde7e2ce7fc
quinnpicard <103086311+quinnpicard@users.noreply.github.com>
2023-03-19 17:13:59 -0700

    fixed version of data_loader_2

--- data_loader_2.py
+++ data_loader_2.py
@@ -1,6 +1,31 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import tensorflow.keras as keras
+import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import uproot
+import utils
+import yaml
+
+with open('definitions_image.yml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    definitions = yaml.load(file, Loader=yaml.FullLoader)
+    
+features = definitions['features']
+spectators = definitions['spectators']
+labels = definitions['labels']
+
+nfeatures = definitions['nfeatures']
+nspectators = definitions['nspectators']
+nlabels = definitions['nlabels']
+    
+feature_array, y, spec_array = utils.get_features_labels('root://eospublic.cern.ch//eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/train/ntuple_merged_10.root', features, spectators, labels, remove_mass_pt_window=False, entry_stop=10000)
+# make image
+X = utils.make_image(feature_array)
+# image is a 4D tensor (n_samples, n_pixels_x, n_pixels_y, n_channels)
+
+# load testing file
+feature_array_test, label_array_test, spec_array_test = utils.get_features_labels('root://eospublic.cern.ch//eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_0.root', features, spectators, labels, remove_mass_pt_window=False)
+                      
+# make image
+X_test = utils.make_image(feature_array_test)
